a005c22bac78daaa2c75c0629be1b84fa855802d
Idan Simantov <idan052545@gmail.com>
2022-03-08 18:10:45 +0200

    adding a header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/cHomepage";
 import ShopPage from "./pages/shop/cShop";
+import Header from "./components/header/cHeader.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
